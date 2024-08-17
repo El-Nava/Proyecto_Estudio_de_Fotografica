324fd3d370977c51241b59d845ae46e8d31e6679
jeesu <jeesu@NAVA>
2024-08-17 12:08:20 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_Estudio_de_Fotografica/Form1.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_Estudio_de_Fotografica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
